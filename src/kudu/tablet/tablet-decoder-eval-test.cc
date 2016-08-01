367563f4caab5c9dafe620d8c04e6aa7f755bc6a
Andrew Wong <andrew.wong@cloudera.com>
2016-08-01 16:51:41 -0700

    Multi-Tablet checkpoint
    Testing using multiple tablets has proven ineffective, whether due
    to having shared_ptr complaining, or to UPSERTs not inserting to
    CFiles, or to the necessity of unique keys for a single FsManager
    and Tablet.
    
    Instead, move to an implementation that creates one tablet per test.
    This will not be as easily tunable, but it should do the trick.

--- src/kudu/tablet/tablet-decoder-eval-test.cc
+++ src/kudu/tablet/tablet-decoder-eval-test.cc
@@ -21,81 +21,75 @@ enum Setup {
   ALL_ON_DISK
 };
 
-class TabletDecoderEvalTest : public KuduMultiTabletTest,
+class TabletDecoderEvalTest : public KuduTabletTest,
                               public ::testing::WithParamInterface<Setup> {
 public:
   // Creates a new tablet for every cardinality being tested
   // This is so that each can be evaluated on a dataset specific to the cardinality
   TabletDecoderEvalTest()
-    : KuduMultiTabletTest(Schema({ColumnSchema("key", INT32),
-                                  ColumnSchema("int_val", INT32),
-                                  ColumnSchema("string_val", STRING)}, 1), 13),
+    : KuduTabletTest(Schema({ColumnSchema("key", INT32),
+                             ColumnSchema("int_val", INT32),
+                             ColumnSchema("string_val", STRING, false, NULL, NULL,
+                             ColumnStorageAttributes(DICT_ENCODING, DEFAULT_COMPRESSION)) }, 1)),
       cardinalities_({5, 10, 15, 20, 30, 40, 60, 80, 100, 200, 400, 500, 1000}),
       num_tablets_(cardinalities_.size()) {
   }
 
   virtual void SetUp() OVERRIDE {
-    LOG(INFO) << "SetUp() called from TabletDecoderEvalTest";
-    KuduMultiTabletTest::SetUp();
-
-    FillTestTablets();
+    KuduTabletTest::SetUp();
   }
 
-  void FillTestTablets() {
+  void FillTestTablet(size_t cardinality) {
     RowBuilder rb(client_schema_);
-
     nrows_ = 2100;
     if (AllowSlowTests()) {
         nrows_ = 100000;
     }
+    // Create new tablet/replace old tablet
+    LocalTabletWriter writer(tablet().get(), &client_schema_);
+    KuduPartialRow row(&client_schema_);
+    for (int64_t i = 0; i < nrows_; i++) {
+      CHECK_OK(row.SetInt32(0, i));
+      CHECK_OK(row.SetInt32(1, i * 10));
+      CHECK_OK(row.SetStringCopy(2, StringPrintf("%08" PRId64, i % cardinality)));
+      ASSERT_OK_FAST(writer.Insert(row));
 
-    for (int t = 0; t < cardinalities_.size(); t++) {
-      // Create new tablet/replace old tablet
-      LocalTabletWriter writer(tablet(t).get(), &client_schema_);
-      KuduPartialRow row(&client_schema_);
-      const size_t cardinality = cardinalities_[t];
-      for (int64_t i = 0; i < nrows_; i++) {
-        CHECK_OK(row.SetInt32(0, i));
-        CHECK_OK(row.SetInt32(1, i * 10));
-        CHECK_OK(row.SetStringCopy(2, StringPrintf("%08" PRId64, i % cardinality)));
-        ASSERT_OK_FAST(writer.Insert(row));
-
-        if (i == 205 && GetParam() == SPLIT_MEMORY_DISK) {
-          ASSERT_OK(tablet(t)->Flush());
-        }
-      }
-
-      if (GetParam() == ALL_ON_DISK) {
-        ASSERT_OK(tablet(t)->Flush());
+      if (i == 205 && GetParam() == SPLIT_MEMORY_DISK) {
+        ASSERT_OK(tablet()->Flush());
       }
     }
+    if (GetParam() == ALL_ON_DISK) {
+      ASSERT_OK(tablet()->Flush());
+    }
+
   }
 
   // Determines the number of expected results assuming predicate of [0, pred_upper)
   // Evaluates the predicate specified by spec and compares the results
-  void TestScanYieldsExpectedResults(ScanSpec spec, uint32_t pred_upper) {
+  void TestScanYieldsExpectedResults(ScanSpec spec, size_t pred_upper) {
     Arena arena(128, 1028);
     AutoReleasePool pool;
     spec.OptimizeScan(schema_, &arena, &pool, true);
+    ScanSpec orig_spec = spec;
 
     for (size_t i = 0; i < cardinalities_.size(); i++) {
       // Open a new tablet and fill it with the correct data
-      // TabletReOpen()
-      // FillTestTablet()
+      FillTestTablet(cardinalities_[i]);
 
-
-      size_t expected_sel_count = pred_upper > num_tablets_ ?
+      size_t expected_sel_count = pred_upper > cardinalities_[i] ?
                                nrows_ :
-                               nrows_ / pred_upper * cardinalities_[i] + nrows_ % cardinalities_[i];
+                               (nrows_ / cardinalities_[i]) * pred_upper + std::min(nrows_ % cardinalities_[i], pred_upper);
       gscoped_ptr<RowwiseIterator> iter;
-      ASSERT_OK(tablet(i)->NewRowIterator(client_schema_, &iter));
+      ASSERT_OK(tablet()->NewRowIterator(client_schema_, &iter));
+      spec = orig_spec;
       ASSERT_OK(iter->Init(&spec));
       ASSERT_TRUE(spec.predicates().empty()) << "Should have accepted all predicates";
 
       int fetched = 0;
-      LOG_TIMING(INFO, "Filtering by int value") {
+      LOG_TIMING(INFO, "Filtering by string value") {
         ASSERT_OK(SilentIterateToStringList(iter.get(), fetched));
       }
+      LOG(INFO) << "Cardinality: " << cardinalities_[i] << ", Expected: " << expected_sel_count << ", Actual: " << fetched;
       ASSERT_EQ(expected_sel_count, fetched);
 
       int expected_blocks_from_disk;
@@ -149,7 +143,7 @@ public:
 
     Schema empty_schema(std::vector<ColumnSchema>(), 0);
     gscoped_ptr <RowwiseIterator> iter;
-    ASSERT_OK(tablet(0)->NewRowIterator(empty_schema, &iter));
+    ASSERT_OK(tablet()->NewRowIterator(empty_schema, &iter));
     ASSERT_OK(iter->Init(&spec));
     ASSERT_TRUE(spec.predicates().empty()) << "Should have accepted all predicates";
 
@@ -166,6 +160,7 @@ private:
   size_t num_tablets_;
   size_t nrows_;
 };
+
 
 TEST_P(TabletDecoderEvalTest, TestMultiTabletBenchmark) {
   // Push down a double-ended range on the integer value column.
@@ -189,6 +184,30 @@ TEST_P(TabletDecoderEvalTest, TestMultiTabletBenchmark) {
   // TODO: collect IO statistics per column, verify that most of the string blocks
   // were not read.
 }
+
+TEST_P(TabletDecoderEvalTest, TestMultiTabletBenchmarkNext) {
+  // Push down a double-ended range on the integer value column.
+
+  ScanSpec spec;
+  const std::string lower_string = StringPrintf("%08" PRId64, static_cast<int64_t>(0));
+  const std::string upper_string = StringPrintf("%08" PRId64, static_cast<int64_t>(21));
+  Slice lower(lower_string);
+  Slice upper(upper_string);
+
+  auto string_pred = ColumnPredicate::Range(schema_.column(2), &lower, &upper);
+  spec.AddPredicate(string_pred);
+
+  // Performs a scan on the data with bounds string( [0,21) )
+  TestScanYieldsExpectedResults(spec, 22);
+
+  // TODO: support non-key predicate pushdown on columns which aren't
+  // part of the projection. The following line currently would crash.
+  // TestCountOnlyScanYieldsExpectedResults(spec);
+
+  // TODO: collect IO statistics per column, verify that most of the string blocks
+  // were not read.
+}
+
 
 INSTANTIATE_TEST_CASE_P(AllDisk, TabletDecoderEvalTest, ::testing::Values(ALL_ON_DISK));
 
